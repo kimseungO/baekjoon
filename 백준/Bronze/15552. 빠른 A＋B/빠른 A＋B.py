b81194946ffd2d995560f56afdaba9263c3fde93
SeungO Kim <105414449+kimseungO@users.noreply.github.com>
2024-02-26 01:30:59 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1464 ms, Memory: 69808 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -0,0 +1,10 @@
+import sys
+input= sys.stdin.readline
+a= int(input())
+
+sum= []
+for i in range(a):
+    num1, num2= map(int,input().split())
+    sum.append(num1+num2)
+for i in range(a):
+    print(sum[i])
