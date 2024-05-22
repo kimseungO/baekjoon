0c722b0e80d7b1a202378c476405faff60496767
SeungO Kim <rtd7878@gmail.com>
2024-05-22 16:42:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,10 +1,7 @@
-baekjoon=list(map(str,input()))
-atoz='abcdefghijklmnopqrstuvwxyz'
-atoz=list(map(str,atoz))
-solut=[]
-for i in atoz:
-    if i in baekjoon:
-        solut.append(baekjoon.index(i))
-    else:
-        solut.append(-1)
-print(*solut)
+word = input()
+
+stk=[-1]*26
+for i in range(26):
+    if chr(i+97) in word:
+        stk[i] = word.index(chr(i+97))
+print(*stk)
