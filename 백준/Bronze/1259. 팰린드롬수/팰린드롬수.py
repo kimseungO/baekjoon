abf997425cd002ee6e70ca2d4bb7f4ee0e53ed28
SeungO Kim <rtd7878@gmail.com>
2024-08-01 13:43:14 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,16 +1,8 @@
-n = 1
 while True:
-    n = str(input())
-    lengs = len(n)
-    leng = lengs//2
-
-    half_n = n[:leng]
-    half_n = half_n[::-1]
-    if lengs%2 ==1:
-        leng+=1
-    if n =='0':
+    num = str(input())
+    if num =='0':
         break
-    elif half_n == n[leng:]:
+    elif num == num[::-1]:
         print('yes')
     else:
         print('no')
