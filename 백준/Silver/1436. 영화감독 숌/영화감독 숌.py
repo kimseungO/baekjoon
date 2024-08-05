27058dbf9d9cab581a030ba5a4d3c23e78b3b24d
SeungO Kim <rtd7878@gmail.com>
2024-08-05 16:06:24 +0900

    [Silver V] Title: 영화감독 숌, Time: 516 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,11 +1,11 @@
 n = int(input())
 
-cnt=0
+cnt=666
 series=0
 while 1:
-    cnt +=1
-    if str(666) in str(cnt):
+    if '666' in str(cnt):
         series +=1
         if series ==n:
             print(cnt)
             break
+    cnt +=1
