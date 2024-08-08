d9cec2742badc56f2f219da4da9e94cc20431a21
SeungO Kim <rtd7878@gmail.com>
2024-08-08 13:50:07 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,10 @@
+n = int(input())
+word = list(input())
+r = 31
+M = 1234567891
+wordnum = []
+for i in range(n):
+    num = (ord(word[i])-ord('a')+1) *(r**(i))
+    wordnum.append(num)
+
+print(sum(wordnum))
