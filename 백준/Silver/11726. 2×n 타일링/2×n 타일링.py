9d60da5d6985d747da01d1a46b0045546d11fb84
SeungO Kim <rtd7878@gmail.com>
2024-09-04 13:54:20 +0900

    [Silver III] Title: 2×n 타일링, Time: 56 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+dp = deque()
+n = int(input())
+
+dp.append(0)
+dp.append(1)
+dp.append(2)
+
+for i in range(3, n+1):
+    dp.append(dp[i-1]+dp[i-2])
+print(dp[n]%10007)
