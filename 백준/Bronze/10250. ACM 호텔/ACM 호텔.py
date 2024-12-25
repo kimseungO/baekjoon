797cd6ca835361008774877249b611057ff8514d
SeungO Kim <rtd7878@gmail.com>
2024-12-25 21:35:48 +0900

    [Bronze III] Title: ACM 호텔, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,21 +1,10 @@
-T=int(input())
-T_list=[]
-for i in range(T):  #값 입력 받음
-    H,W,N=map(int,input().split())
-    W=list(range(1,W+1))
-    hotel=[]
-    new_hotel=[]
-    for i in W:  #층을 문자형으로 hotel리스트에 저장
-        a=str(i)
-        if len(a)<2:
-            hotel.append(str(0)+a)
-            continue
-        hotel.append(a)
-    for i in hotel:  #hotel리스트를 new_hotel로 호수 채워주기
-        for j in range(1,H+1):
-            a=str(j)+str(i)
-            new_hotel.append(a)
-    new_hotel=[int(x) for x in new_hotel]
-    T_list.append(new_hotel[N-1])
+T = int(input())
+
 for i in range(T):
-    print(T_list[i])
+    H,W,N = map(int,input().split())
+    X = N % H
+    Y = N // H
+    if X == 0:
+        print(H * 100 + Y)
+    else:
+        print((X*100)+(Y+1))
